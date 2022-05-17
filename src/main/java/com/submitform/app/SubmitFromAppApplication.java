3879532a0d52e8e9c67ddcaeb8ca85088b01e63d
mdasi <mdasi@LAPTOP-L5PK7RPA>
2022-05-17 20:00:11 +0530

    register jsp

--- src/main/java/com/submitform/app/SubmitFromAppApplication.java
+++ src/main/java/com/submitform/app/SubmitFromAppApplication.java
@@ -2,9 +2,17 @@ package com.submitform.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SubmitFromAppApplication {
+public class SubmitFromAppApplication /* extends SpringBootServletInitializer */ {
+	 
+	/*
+	 * @Override protected SpringApplicationBuilder
+	 * configure(SpringApplicationBuilder application) { return
+	 * application.sources(SubmitFromAppApplication.class); }
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(SubmitFromAppApplication.class, args);
--- src/main/java/com/submitform/app/controller/RegisterController.java
+++ src/main/java/com/submitform/app/controller/RegisterController.java
@@ -1,15 +1,15 @@
 package com.submitform.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.submitform.app.dto.Persons;
 import com.submitform.app.dto.RegisterDetails;
 import com.submitform.app.service.impl.UserServiceImpl;
 
-@RestController
+@Controller
 public class RegisterController {
 	
 	@Autowired
@@ -20,8 +20,8 @@ public class RegisterController {
 	public String register() {
 		Persons person=userService.findPersonById(12546);
 		System.out.println("person found="+person);
-		
 		return "register";
+		
 	}
 	
 	
